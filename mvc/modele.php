c162aeb9e8ec5d58d55299647919ab9b37d04bf6
NDeicke <julien.fouquet.85@gmail.com>
2012-10-04 11:16:37 +0200

    fichier modele.php commenté

--- mvc/modele.php
+++ mvc/modele.php
@@ -1,21 +1,21 @@
 <?php
 
 class MVC_Modele {
-
     static private $_pdo;
     protected $_modeleEnregistrement='MVC_ModeleEnregistrement';
     
     /**
-     * Connexion à la base de données via PDO
-     * @return type
+     * Connecte l'application a la base de donnees
+     * Les variables utiles a la connexion se trouve dans public/params/params_chemins.php
+     * @return type : connexion 
      */
     static function pdo() {
         if(is_null(self::$_pdo)){
-            $dsn = 'mysql:dbname='.Params_BDD::BDD.';host='.Params_BDD::HOST.'';
+            $dsn = 'mysql:dbname='.Params_BDD::BDD.';host='.Params_BDD::HOST;
             $options = array(PDO::MYSQL_ATTR_INIT_COMMAND=> 'SET NAMES utf8');
             $user = Params_BDD::USER;
             $password = Params_BDD::PWD;
-
+            
             try {
                 self::$_pdo= new PDO($dsn, $user, $password, $options);
             } catch (PDOException $e) {
@@ -23,44 +23,41 @@ class MVC_Modele {
             }
         }
         return self::$_pdo;
-    }
+    }  
     
     /**
-     * Liste de tous les enregistrements d'une table
-     * @return type
+     *
+     * @param string - $where : clause where de la requete permettant de restreindre la recherche
+     * valeur par defaut : '1=1'
+     * @param tableau de parametres - $params : valeur permettant de restreindre la recherche selon le type de restriction choisi avec $where
+     * valeur par defaut : 'array()'
+     *      lorsque where() est appele sans specifier les parametres, la fonction retourne tous les resultats de la table
+     *      on peut egalement ne preciser que le premier parametre
+     *          exemple : recuperer tous les resultats mais par ordre croissant/alphabétique, ou avec une limite du nombre de resultats
+     * @return enregistrements (object) 
      */
-    function liste() {
-        return $this->where('1=1',array());
-    }
-    
-    /**
-     * Liste les enregistrements d'une table avec clause WHERE (condition)
-     * @param type $where
-     * @param type $params
-     * @return type
-     */
-    function where($where,$params){
+    function where($where = '1=1',$params = array()){
         $query = 'select * from '.$this->_table. ' where '.$where;
         $result=self::pdo()->prepare($query);
         $result->execute($params);
         return $result->fetchAll(PDO::FETCH_CLASS, $this->_modeleEnregistrement);
+
+        //PDO::FETCH_CLASS : permet de renvoyer les resultats sous forme d'objets plutot qu'un tableau
     }
     
     /**
-     * Retourne le premier enregistrement de la requête
-     * @param string $where
-     * @param array $params
-     * @return \modele_enregistrement
+     * Retourne le premier enregistrement de la requete
+     * @param cf where()
+     * @return enregistrement (object)
      */
-    function whereFirst($where,$params){
+    function whereFirst($where ,$params){
         $liste=$this->where($where,$params);
         return $result[0];
     }
-    
     /**
-     * Retourne un enregistrement par son id
-     * @param type $id
-     * @return type
+     * Retourne un enregistrement, precise par un id
+     * @param int - $id : id de l'enregistrement a recuperer
+     * @return enregistrement (object)
      */
     function get($id){
         $result=$this->where('id=?',array($id));
@@ -68,8 +65,8 @@ class MVC_Modele {
     }
     
     /**
-     * retourne un item vide (pour la création)
-     * @return \modele_enregistrement
+     * Retourne un item vide (pour la création)
+     * @return modeleenregistrement (object)
      */
     function newEnregistrement() {
         $item = new $this->_modele_enregistrement();
@@ -84,9 +81,9 @@ class MVC_Modele {
     }
     
     /**
-     * retourne vrai si l'id existe
-     * @param int $valeurClePrimaire
-     * @return bool
+     * Permet de s'avoir si un id existe ou non vrai si l'id existe
+     * @param int - $valeurClePrimaire : id dont on souhaite connaitre l'existence
+     * @return vrai si l'id existe, sinon faux
      */
     function exists($valeurClePrimaire) { 
         return sizeof($this->where('id=?',array($valeurClePrimaire))) > 0;
