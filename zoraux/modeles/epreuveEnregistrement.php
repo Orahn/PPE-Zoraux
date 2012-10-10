11e6f6bce7e4a85dd258edfe8b175dc6add0426e
NDeicke <natacha.deicke@gmail.com>
2012-10-10 18:39:55 +0200

    creation du modele epreuveEnregistrement

--- zoraux/modeles/epreuveEnregistrement.php
+++ zoraux/modeles/epreuveEnregistrement.php
@@ -1,5 +1,15 @@
 <?php
 
 class Zoraux_Modeles_EpreuveEnregistrement extends MVC_ModeleEnregistrement  {
-    
+    protected $_table='epreuve';
+    /**
+     * Retourne un enregistrement de la table classe selon la valeur de la cle etrangere de la table epreuve passe en parametre dans get()
+     * @return type : enregistrement (objet) de la table classe
+     */
+    function classe(){
+        $tableClasses=new Zoraux_Modeles_Classe();
+        $classe=$tableClasses->get($this->classe_id);
+        return $classe;
+        
+    }
 }
