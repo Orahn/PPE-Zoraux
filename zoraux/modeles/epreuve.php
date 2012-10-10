1eb1f6d1af6aa11a3fe15e062ea08fc6619177bf
NDeicke <natacha.deicke@gmail.com>
2012-10-10 18:44:07 +0200

    creation du modele Epreuve

--- zoraux/modeles/epreuve.php
+++ zoraux/modeles/epreuve.php
@@ -1,5 +1,21 @@
 <?php
 
 class Zoraux_Modeles_Epreuve extends MVC_Modele {
-    
+    protected $_table = 'epreuve';
+    protected $_modeleEnregistrement='Zoraux_Modeles_EpreuveEnregistrement';
+
+    /**
+     * Cree un nouvel epreuve dont les champs sont vides, afin de simuler une modification dans le formulaire
+     * @return Zoraux_Modeles_EpreuveEnregistrement : Nouvel epreuve vide cree
+     */
+    function newEpreuve(){
+        $epreuve = new Zoraux_Modeles_EpreuveEnregistrement();
+        $epreuve->id=null;
+        $epreuve->libelle=null;
+        $epreuve->dureeLibre=null;
+        $epreuve->dureePassage=null;
+        $epreuve->dureePreparation=null;
+        $epreuve->classe_id=null;
+        return $epreuve;
+    }
 }
