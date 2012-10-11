f5ffd0b999e89d399289ccc707c64c4c80936202
NDeicke <natacha.deicke@gmail.com>
2012-10-11 10:27:22 +0200

    ajout de la méthode listeSalles()

--- zoraux/modeles/salleEnregistrement.php
+++ zoraux/modeles/salleEnregistrement.php
@@ -2,4 +2,12 @@
 
 class Zoraux_Modeles_SalleEnregistrement extends MVC_ModeleEnregistrement  {
     protected $_table='salle';
+    /**
+     * Recupere la liste de toutes les classes
+     */
+    function listeSalles(){
+        $tableSalles=new Zoraux_Modeles_Salle();
+        $salles=$tableSalles->liste();
+        $this->vue->salles=$salles;
+    }
 }
