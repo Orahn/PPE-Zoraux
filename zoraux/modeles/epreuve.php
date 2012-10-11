5ffe6134b34703d6dd471648cd1f90c891e754e5
NDeicke <natacha.deicke@gmail.com>
2012-10-11 09:53:05 +0200

    commentaire retiré dans newEpreuve()

--- zoraux/modeles/epreuve.php
+++ zoraux/modeles/epreuve.php
@@ -6,17 +6,10 @@ class Zoraux_Modeles_Epreuve extends MVC_Modele {
 
     /**
      * Cree une nouvelle epreuve dont les champs sont vides, afin de simuler une modification dans le formulaire
-     * @return Zoraux_Modeles_EpreuveEnregistrement : Nouvel epreuve vide cree
+     * @return Zoraux_Modeles_EpreuveEnregistrement : Nouvelle epreuve vide cree
      */
     function newEpreuve(){
         $epreuve = $this->newEnregistrement();
-        /*$epreuve = new Zoraux_Modeles_EpreuveEnregistrement();
-        $epreuve->id=null;
-        $epreuve->libelle=null;
-        $epreuve->dureeLibreAvant=null;
-        $epreuve->dureePassage=null;
-        $epreuve->dureePreparation=null;
-        $epreuve->classe_id=null;*/
         return $epreuve;
     }
 }
