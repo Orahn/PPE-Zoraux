47902157e87d2aa69258f17c8ffd45988fb0aaaa
Edouard FORGEAU <edouard.forgeau@hotmail.fr>
2012-10-11 11:53:24 +0200

    getClasse();

--- zoraux/modeles/eleveEnregistrement.php
+++ zoraux/modeles/eleveEnregistrement.php
@@ -2,4 +2,14 @@
 
 class Zoraux_Modeles_EleveEnregistrement extends MVC_ModeleEnregistrement  {
     protected $_table='eleve';
+    
+    /**
+     * Permet de récupérer la classe de l'élève
+     * @return classe
+     */
+    function getClasse(){
+        $tableClasses=new Zoraux_Modeles_ClasseEnregistrement();
+        $classe=$tableClasses->get($this->classe_id);
+        return $classe;
+    }
 }
