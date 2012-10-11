2aebf3fba90838e58212563e6c4d1b126ee1a975
usio <usio@usio-124-ubu11.(none)>
2012-10-11 11:33:10 +0200

    Base de modele et controleur de Classe

--- zoraux/controleurs/classe.php
+++ zoraux/controleurs/classe.php
@@ -2,4 +2,25 @@
 
 class Zoraux_Controleurs_Classe {
     
+    function listeProfesseur(){
+        $tableProfesseurs=new Zoraux_Modeles_MembreJury();
+        $id=$_GET['id'];
+        $professeur=$tableProfesseurs->get($id);
+        $this->vue->auteur=$professeur;
+        $tableClasses=new Zoraux_Modeles_Classe();
+        $classes=$tableClasses->where('id=?',array($id));
+        $this->vue->articles=$classes;
+    }
+    
+    /**
+     * Permet de supprimer une classe de la base de donnees
+     */
+    
+    function supprimerClasse(){
+        $tableClasses=new Zoraux_Modeles_Classe();
+        if(isset($_GET['id'])){ //Si l'id existe dans l'URL
+            $classe=$tableClasses->get($_GET['id']); //On recupere l'article correspondant à cet id dans la table article
+            $classe->supprimer(); //On fait appel à la methode supprimer sur cet article
+        }
+    }
 }
--- zoraux/modeles/classe.php
+++ zoraux/modeles/classe.php
@@ -1,5 +1,6 @@
 <?php
 
 class Zoraux_Modeles_Classe extends MVC_Modele {
-    
+    protected $_table='classe';
+    protected $_modeleEnregistrement='Zoraux_Modeles_ClasseEnregistrement';
 }
