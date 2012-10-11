736c6438a69c52e236efa7b10acbd8a6ef2f3aa2
NDeicke <natacha.deicke@gmail.com>
2012-10-11 11:16:39 +0200

    création du modèle membreJury

--- zoraux/modeles/membreJury.php
+++ zoraux/modeles/membreJury.php
@@ -1,5 +1,15 @@
 <?php
 
 class Zoraux_Modeles_MembreJury extends MVC_Modele {
-    
+    protected $_table = 'membreJury';
+    protected $_modeleEnregistrement='Zoraux_Modeles_MembreJuryEnregistrement';
+
+    /**
+     * Cree un nouveau membre du jury dont les champs sont vides, afin de simuler une modification dans le formulaire
+     * @return Zoraux_Modeles_MembreJuryEnregistrement : Nouveau membre du jury vide cree
+     */
+    function newMembreJury(){
+        $membreJury = $this->newEnregistrement();
+        return $membreJury;
+    }
 }
