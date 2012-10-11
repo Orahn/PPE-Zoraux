95c41f8fc3323b3da4109a66626733e95644f7e2
NDeicke <natacha.deicke@gmail.com>
2012-10-11 09:36:27 +0200

    classe() devient getClasse()

--- zoraux/modeles/epreuveEnregistrement.php
+++ zoraux/modeles/epreuveEnregistrement.php
@@ -6,10 +6,9 @@ class Zoraux_Modeles_EpreuveEnregistrement extends MVC_ModeleEnregistrement  {
      * Retourne un enregistrement de la table classe selon la valeur de la cle etrangere de la table epreuve passe en parametre dans get()
      * @return type : enregistrement (objet) de la table classe
      */
-    function classe(){
+    function getClasse(){
         $tableClasses=new Zoraux_Modeles_Classe();
         $classe=$tableClasses->get($this->classe_id);
         return $classe;
-        
     }
 }
