7c186a56fcb719c06448e11f1e327772dda90e40
usig <usig@ubu10-MOD.(none)>
2012-10-11 10:45:59 +0200

    Modeles Eleve

--- zoraux/modeles/eleve.php
+++ zoraux/modeles/eleve.php
@@ -1,5 +1,21 @@
 <?php
 
 class Zoraux_Modeles_Eleve extends MVC_Modele {
-    
+    protected $_table='eleve';
+    protected $_modeleEnregistrement='Zoraux_Modeles_EleveEnregistrement';
+
+    function newEleve(){
+        $eleve = new Zoraux_Modeles_EleveEnregistrement();
+        $eleve->id=null;
+        $eleve->nom='';
+        $eleve->prenom='';
+        $eleve->login='';
+        $eleve->password='';
+        $eleve->adresseRue='';
+        $eleve->adresseVille='';
+        $eleve->adresseCP='';
+        $eleve->email='';
+        $eleve->tiersTemps='0';
+        return $eleve;
+    }
 }
--- zoraux/modeles/eleveEnregistrement.php
+++ zoraux/modeles/eleveEnregistrement.php
@@ -1,5 +1,5 @@
 <?php
 
 class Zoraux_Modeles_EleveEnregistrement extends MVC_ModeleEnregistrement  {
-    
+    protected $_table='eleve';
 }
