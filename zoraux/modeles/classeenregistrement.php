aaaa54478572a69c45f49bd379d83c275894ddfb
Julien FOUQUET <julien.fouquet.85@gmail.com>
2012-10-18 08:22:15 +0200

    Ajout du controleur/modele classe du binome 6

--- zoraux/modeles/classeenregistrement.php
+++ zoraux/modeles/classeenregistrement.php
@@ -1,5 +1,27 @@
 <?php
 
-class Zoraux_Modeles_ClasseEnregistrement extends MVC_ModeleEnregistrement  {
-
+class Zoraux_Modeles_ClasseEnregistrement extends MVC_ModeleEnregistrement{
+    protected $_table='classe';
+    
+     /**
+     * Permet de recuperer les eleves d'une classe
+     * @return array
+     */
+    
+    function getEleve(){
+        $tableEleves=new Zoraux_Modeles_Eleve();
+        $eleve=$tableEleves->get($this->classe_id);
+        return $eleve;
+    }
+    
+    /**
+     * Permet de recuperer les epreuves d'une classe
+     * @return array
+     */
+    
+    function getEpreuve(){
+        $tableEpreuves=new Zoraux_Modeles_Epreuve();
+        $epreuve=$tableEpreuves->get($this->classe_id);
+        return $epreuve;
+    }
 }
