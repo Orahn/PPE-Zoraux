1125b6c83dff34c1495d9559869c22c200faffd7
tarteAuxPommes <usig@ubu10-MOD.(none)>
2012-10-18 10:43:07 +0200

    forulaire

---
+++ zoraux/vues/eleve/formulaireeleve.php
@@ -0,0 +1,17 @@
+<?php
+
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ * Description of eleveenregistrement
+ *
+ * @author usig
+ */
+class eleveenregistrement {
+    //put your code here
+}
+
+?>
