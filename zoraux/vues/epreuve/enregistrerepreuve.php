05f36e6c3c940793fa6c46c2d64277a3a5940e9d
NDeicke <natacha.deicke@gmail.com>
2012-10-25 10:26:20 +0200

    ajout d'une alerte pour montrer que l'épreuve a bien été enregistrée

--- zoraux/vues/epreuve/enregistrerepreuve.php
+++ zoraux/vues/epreuve/enregistrerepreuve.php
@@ -1,4 +1,7 @@
 <?php
     echo $this->templateHaut();
+    echo '<script language="javascript">
+            alert(\'Epreuve enregistr\351e\');
+        </script>';
     echo '<meta http-equiv="Refresh" content="0;URL=index.php?controleur=zoraux_controleurs_accueil&action=principale">';
     echo $this->templateBas();
