3a1daa776a5399a8b92c37a26be55971ba71043d
Alexis LEROUX <alexis.leroux8@laposte.net>
2012-10-25 08:43:07 +0200

    Réparation parenthèse sur if et acolade pour class et function

--- mvc/periode.php
+++ mvc/periode.php
@@ -1,46 +1,48 @@
 <?php
-    class MVC_Periode {
+    class MVC_Periode 
+    {
         private $periode;
         /**
          * Constructeur de la classe periode (calcul la difference de temps entre deux dates)
          * @param MVC_Date $date1 recupere une premiere date de la classe date
          * @param MVC_Date $date2 recupere une seconde date de la classe date
          */
-        function __construct(MVC_Date $date1,  MVC_Date $date2) {
+        function __construct(MVC_Date $date1,  MVC_Date $date2) 
+        {
             $this->periode=$date1->diff($date2);
         }
         /**
          * Renvoie la periode entre deux dates (de la classe date)
          * @return string une chaine indiquant la periode
          */
-        public function getPeriode(){
-            
+        public function getPeriode() 
+        {
             $string='';
-            if($this->periode->y==1){
+            if($this->periode->y==1) {
                 $string.=$this->periode->y.' an ';
             }
-            if($this->periode->y>1){
+            if($this->periode->y>1) {
                 $string.=$this->periode->y.' ans ';    
             }
-            if($this->periode->m>=1){
+            if($this->periode->m>=1) {
                 $string.=$this->periode->m.' mois ';    
             }
-            if($this->periode->d==1){
+            if($this->periode->d==1) {
                 $string.=$this->periode->d.' jour ';
             }
-            if($this->periode->d>1){
+            if($this->periode->d>1) {
                 $string.=$this->periode->d.' jours ';
             }
-            if($this->periode->h==1){
+            if($this->periode->h==1) {
                 $string.=$this->periode->h.' heure ';
             }
-            if($this->periode->h>1){
+            if($this->periode->h>1) {
                 $string.=$this->periode->h.' heures ';
             }
-            if($this->periode->i==1){
+            if($this->periode->i==1) {
                 $string.=$this->periode->i.' minute ';
             }
-            if($this->periode->i>1){
+            if($this->periode->i>1) {
                 $string.=$this->periode->i.' minutes ';
             }
             return $string;
